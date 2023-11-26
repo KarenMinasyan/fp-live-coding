55ce2816840a4f650e6f3f6212a53797c48d9ed8
Karen Minasyan <kminasyantricking@gmail.com>
2023-11-26 18:53:07 +0400

    init fp imperative

---
+++ README.md
@@ -0,0 +1,1 @@
+ramda.js - it works iteration first data last principe and in there all functions curried
---
+++ src/helpers/index.js
@@ -0,0 +1,33 @@
+import fs from 'fs';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import chalk from 'chalk';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export const readFile = (filename) => {
+  const filePath = path.resolve(__dirname, '../', filename);
+
+  return fs.readFileSync(filePath, { encoding: 'utf8' });
+};
+
+export const writeFile = (filename, filepath, data) => {
+  const filePath = path.join(__dirname, '../', filepath, filename);
+
+  fs.writeFileSync(filePath, data);
+};
+
+const printToConsole = console.log;
+
+export const log = (color, title, value) => {
+  if (typeof value !== 'string') {
+    value = JSON.stringify(value);
+  }
+
+  printToConsole(
+    `${chalk[color](`[${title.toUpperCase()}] `)}${chalk.white(
+      value.replace('\n', ''),
+    )}`,
+  );
+};
